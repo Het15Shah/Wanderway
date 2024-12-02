5aab75e7ce092eeb60dcd5f923119f2b6f16d7ab
HetShah1205 <shahhet525@gmail.com>
2024-12-02 18:19:38 +0530

    changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,22 +9,25 @@ import SignUpPage from "./pages/Signup";
 import Addtrips from "./pages/Addtrips";
 import ReviewPage from "./pages/Reviewpage";
 import MyTrip from "./pages/MyTrip";
-import CustomItineraryForm from "./pages/customtrip"; // Ensure this import is correct
+import CustomItineraryForm from "./pages/customtrip";
+import ProtectedRoute from "./pages/ProtectedRoute";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/profile" element={<ProtectedRoute Component={ProfilePage} />} />
         <Route path="/about-us" element={<AboutUs />} />
         <Route path="/trip/:id" element={<TripDetail />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/add-trips" element={<Addtrips />} />
+        <Route path="/add-trips" element={<ProtectedRoute Component={Addtrips} />} />
         <Route path="/review" element={<ReviewPage />} />
-        <Route path="/my-trips" element={<MyTrip />} />
-        <Route path="/create" element={<CustomItineraryForm />} /> {/* Correct route */}
+        <Route path="/my-trips" element={<ProtectedRoute Component={MyTrip} />} />
+        <Route path="/create" element={<ProtectedRoute Component={CustomItineraryForm} />} /> {/* Correct route */}
+        <Route path="*" element={<PageNotFound />} /> {/* Correct route */}
       </Routes>
     </>
   );
